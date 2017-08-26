47929f9191320df39019086168bc4e3a2e7bd23a
AmyOrz <340606700@qq.com>
2017-08-26 16:54:15 +0800

    chore:last commit forget commit "change ISceneGraph", now commit it

--- src/mainEditor/logic/interface/ISceneGraph.ts
+++ src/mainEditor/logic/interface/ISceneGraph.ts
@@ -3,34 +3,34 @@ export interface ISceneGraph {
     triangle: IGameObject;
 }
 
-export interface IGameObject{
-    children?:IGameObject[];
-    component:IComponent;
+export interface IGameObject {
+    children?: IGameObject[];
+    component: IComponent;
 }
 
-export interface IComponent{
-    transform:ITransform;
-    material?:IMaterial;
-    camera?:ICamera;
+export interface IComponent {
+    transform: ITransform;
+    material?: IMaterial;
+    camera?: ICamera;
 }
 
-export interface ITransform{
-    rotate:number[];
-    position:number[];
-    scale:number[];
+export interface ITransform {
+    rotate: number[];
+    position: number[];
+    scale: number[];
 }
 
-export interface ICamera{
-    projection:"Perspective"|"Orthographic";
-    far:number;
-    near:number;
-    aspect:number;
-    fovy:number;
+export interface ICamera {
+    projection: "Perspective" | "Orthographic";
+    far: number;
+    near: number;
+    aspect: number;
+    fovy: number;
 }
 
-export interface IMaterial{
-    color:string|number[];
-    opacity:number;
+export interface IMaterial {
+    color: string | number[];
+    opacity: number;
 }
 
 /*
