89c5aa2d14b315973686efe96d5092638ea9a8c4
AmyOrz <340606700@qq.com>
2017-11-27 14:08:03 +0800

    refactor:gulp remove redundance code refactor:package remove jest,instead of wonder-bs-jest

--- gulpfile.js
+++ gulpfile.js
@@ -3,28 +3,28 @@ var gulpSync = require("gulp-sync")(gulp);
 var path = require("path");
 var wonderPackage = require("wonder-package");
 var exec = require('child_process').exec;
-var package = wonderPackage.package;
+var wonderEditorTool = require("wonder-editor-tool");
 
-var convertCssPath = require("./server/convertImportCss");
+var package = wonderPackage.package;
+var convertCssPath = wonderEditorTool.convertImportCss.convert;
 
 
 
 
 gulp.task("compileSass", function (done) {
-    exec("sass --update ./src/:./src/", function(){
+    exec("sass --update ./src/:./src/", function () {
         done();
     });
 });
 
 gulp.task("compileReason", function (done) {
-    exec("npm run build", function(){
+    exec("npm run build", function () {
         done();
     });
 });
 
 gulp.task("rollupProject", function (done) {
-    var filePath = path.resolve(__dirname + "/lib/es6_global");
-
+    var filePath = path.resolve(__dirname + "/lib/es6_global/src");
     convertCssPath(filePath);
 
     package.rollup(path.join(process.cwd(), "./rollup.config.js"), done);
