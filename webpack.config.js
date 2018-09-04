058a5a19af2197d722b3ba325301fceccca03a02
AmyOrz <340606700@qq.com>
2018-09-04 14:36:49 +0800

    docs(webpack): add two mode in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
   entry: {
     wd: './lib/es6_global/src/core/Index.js',
   },
+  mode: isProd ? 'production' : 'development',
   output: {
     path: path.join(__dirname, "dist"),
     filename: 'index.js',
