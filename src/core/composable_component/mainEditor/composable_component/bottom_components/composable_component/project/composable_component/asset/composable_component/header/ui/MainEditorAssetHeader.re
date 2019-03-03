Wonder-Technology/Wonder-Editor

commit 05c98b5fc1c3c769098b888120c1011724c5f29d

feat(hotkey): delete now can remove asset

src/core/composable_component/mainEditor/composable_component/bottom_components/composable_component/project/composable_component/asset/composable_component/header/ui/MainEditorAssetHeader.re
@@ -9,16 +9,6 @@ type action =
   | BlurNav;
 
 module Method = {
-  let isCurrentNodeEqualRootNode = editorState =>
-    switch (editorState |> CurrentNodeIdAssetEditorService.getCurrentNodeId) {
-    | None => true
-    | Some(currentNodeId) =>
-      NodeAssetService.isIdEqual(
-        currentNodeId,
-        RootTreeAssetEditorService.getRootNode(editorState)
-        |> NodeAssetService.getNodeId(~node=_),
-      )
-    };
   let addFolder = AssetHeaderAddFolderEventHandler.MakeEventHandler.pushUndoStackWithNoCopyEngineState;
 
   let removeAssetNode = AssetHeaderRemoveNodeEventHandler.MakeEventHandler.pushUndoStackWithNoCopyEngineState;
@@ -39,13 +29,13 @@ let _renderSelectNav =
   <div className="item-content">
     <div
       className="content-section"
-      onClick=(_e => Method.addFolder((uiState, dispatchFunc), (), ()))>
-      <span className="section-header"> (DomHelper.textEl("Folder")) </span>
+      onClick={_e => Method.addFolder((uiState, dispatchFunc), (), ())}>
+      <span className="section-header"> {DomHelper.textEl("Folder")} </span>
     </div>
     <div
       className="content-section"
-      onClick=(_e => Method.addMaterial((uiState, dispatchFunc), (), ()))>
-      <div className="section-header"> (DomHelper.textEl("Material")) </div>
+      onClick={_e => Method.addMaterial((uiState, dispatchFunc), (), ())}>
+      <div className="section-header"> {DomHelper.textEl("Material")} </div>
     </div>
   </div>;
 
@@ -57,20 +47,22 @@ let _renderRemoveItem =
     ) =>
   <div
     className="asset-header-item"
-    onClick=(
+    onClick={
       _e =>
-        Method.isCurrentNodeEqualRootNode |> StateLogicService.getEditorState ?
+        CurrentNodeIdAssetEditorService.couldRemoveCurrentNode
+        |> StateLogicService.getEditorState ?
           () : Method.removeAssetNode((uiState, dispatchFunc), (), ())
-    )>
-    (
-      Method.isCurrentNodeEqualRootNode |> StateLogicService.getEditorState ?
+    }>
+    {
+      CurrentNodeIdAssetEditorService.couldRemoveCurrentNode
+      |> StateLogicService.getEditorState ?
         <div className="item-notBeClick">
           <img src="./public/img/notRemove.png" />
         </div> :
         <div className="item-canBeClick">
           <img src="./public/img/remove.png" />
         </div>
-    )
+    }
   </div>;
 
 let render =
@@ -79,16 +71,16 @@ let render =
       ({state, send}: ReasonReact.self('a, 'b, 'c)) as self,
     ) =>
   <article key="assetHeader" className="wonder-asset-header">
-    <div className="asset-header-item" onClick=(_e => send(ToggleShowNav))>
+    <div className="asset-header-item" onClick={_e => send(ToggleShowNav)}>
       <div className="item-canBeClick">
         <img src="./public/img/add.png" />
       </div>
-      (
+      {
         state.isSelectNav ?
           _renderSelectNav(uiState, dispatchFunc, self) : ReasonReact.null
-      )
+      }
     </div>
-    (_renderRemoveItem(uiState, dispatchFunc, self))
+    {_renderRemoveItem(uiState, dispatchFunc, self)}
     <div className="asset-header-item">
       <div className="item-canBeClick">
         <img src="./public/img/load.png" />
@@ -96,7 +88,7 @@ let render =
           className="asset-fileLoad"
           type_="file"
           multiple=false
-          onChange=(
+          onChange={
             e =>
               Method.fileLoad(
                 (uiState, dispatchFunc),
@@ -104,7 +96,7 @@ let render =
                 e,
               )
               |> ignore
-          )
+          }
         />
       </div>
     </div>

.cz-config.js
@@ -15,6 +15,7 @@ module.exports = {
   ],
 
   scopes: [
+    { name: "hotkey" },
     { name: "focus" },
     { name: "scene-view" },
     { name: "scene-tree" },

src/core/job/init/InitHotKeysJob.re
@@ -72,7 +72,14 @@ let _getHandleFuncByHotKeyAction = hotKeyAction => {
             (),
             (),
           ) :
-          ()
+          CurrentNodeIdAssetEditorService.couldRemoveCurrentNode
+          |> StateLogicService.getEditorState ?
+            MainEditorAssetHeader.Method.removeAssetNode(
+              (uiState, dispatch),
+              (),
+              (),
+            ) :
+            ()
     )
   | Focus => (
       () => {
@@ -103,15 +110,16 @@ let _preventBrowserHotKeys = event => {
 let _handleHotKeyFunc = hotKeyDataArray =>
   hotKeyDataArray
   |> Js.Array.forEach(((hotKeys, hotKeyAction)) =>
-       hotKeys
-       |> Js.Array.joinWith(",")
-       |. HotKeysJs.hotkeys((e, handler) => {
-            _preventBrowserHotKeys(e);
+       (hotKeys |> Js.Array.joinWith(","))
+       ->(
+           HotKeysJs.hotkeys((e, handler) => {
+             _preventBrowserHotKeys(e);
 
-            let handleFunc = _getHandleFuncByHotKeyAction(hotKeyAction);
+             let handleFunc = _getHandleFuncByHotKeyAction(hotKeyAction);
 
-            handleFunc();
-          })
+             handleFunc();
+           })
+         )
      );
 
 let initHotKeysForEditorJob = (_, engineState) => {

src/service/state/editor/asset/CurrentNodeIdAssetEditorService.re
@@ -18,3 +18,15 @@ let setCurrentNodeId = (currentNodeId, editorState) => {
     editorState.assetRecord
     |> CurrentNodeIdAssetService.setCurrentNodeId(currentNodeId),
 };
+
+let couldRemoveCurrentNode = editorState =>
+  switch (editorState |> getCurrentNodeId) {
+  | None => false
+  | Some(currentNodeId) =>
+    !
+      NodeAssetService.isIdEqual(
+        currentNodeId,
+        RootTreeAssetEditorService.getRootNode(editorState)
+        |> NodeAssetService.getNodeId(~node=_),
+      )
+  };

test/integration/job/__snapshots__/initHotKeysJob_test.js.snap
@@ -132,7 +132,41 @@ exports[`init hotKeys job bind document hotKeys event, need add hot-key into Set
 </article>
 `;
 
-exports[`init hotKeys job bind document hotKeys event, need add hot-key into SettingTool test bind delete hot-key key down delete,if has currentSceneTreeNode, should delete it 1`] = `
+exports[`init hotKeys job bind document hotKeys event, need add hot-key into SettingTool test bind delete hot-key key down delete else if has current asset tree node, should delete it 1`] = `
+<article
+  className="wonder-asset-assetTree"
+>
+  <ul
+    className="wonder-tree-node"
+  >
+    <li>
+      <div
+        className="item-triangle"
+      />
+      <img
+        className="treeNode-icon"
+        src="./public/img/package.png"
+      />
+      <div
+        className="draggable-container select-not-active"
+        draggable={true}
+        onClick={[Function]}
+        onDragEnd={[Function]}
+        onDragEnter={[Function]}
+        onDragLeave={[Function]}
+        onDragOver={[Function]}
+        onDragStart={[Function]}
+        onDrop={[Function]}
+        style={Object {}}
+      >
+        Assets
+      </div>
+    </li>
+  </ul>
+</article>
+`;
+
+exports[`init hotKeys job bind document hotKeys event, need add hot-key into SettingTool test bind delete hot-key key down delete if has currentSceneTreeNode, should delete it 1`] = `
 <article
   className="wonder-sceneTree-component"
   id="wonder-sceneTree-component"

test/integration/job/initHotKeysJob_test.re
@@ -160,38 +160,62 @@ let _ =
       );
 
       describe("test bind delete hot-key", () =>
-        test(
-          "key down delete,if has currentSceneTreeNode, should delete it", () => {
-          MainEditorSceneTool.setFirstCubeToBeCurrentSceneTreeNode();
+        describe("key down delete", () => {
+          test("if has currentSceneTreeNode, should delete it", () => {
+            MainEditorSceneTool.setFirstCubeToBeCurrentSceneTreeNode();
 
-          triggerDeleteHotKeyEvent();
+            triggerDeleteHotKeyEvent();
 
-          BuildComponentTool.buildSceneTree(TestTool.buildEmptyAppState())
-          |> ReactTestTool.createSnapshotAndMatch;
+            BuildComponentTool.buildSceneTree(TestTool.buildEmptyAppState())
+            |> ReactTestTool.createSnapshotAndMatch;
+          });
+          test("else if has current asset tree node, should delete it", () => {
+            GameObjectTool.clearCurrentSceneTreeNode();
+            let assetTreeData =
+              MainEditorAssetTreeTool.BuildAssetTree.Folder.TwoLayer.buildOneFolderAssetTree();
+            MainEditorAssetTreeTool.Select.selectFolderNode(
+              ~nodeId=
+                MainEditorAssetTreeTool.BuildAssetTree.Folder.TwoLayer.getFirstFolderNodeId(
+                  assetTreeData,
+                ),
+              (),
+            );
+
+            triggerDeleteHotKeyEvent();
+
+            BuildComponentTool.buildAssetTree()
+            |> ReactTestTool.createSnapshotAndMatch;
+          });
         })
       );
 
       describe("test bind focus hot-key", () => {
         let _getDistance = ((editorState, engineState)) =>
-          editorState
-          |> SceneViewEditorService.unsafeGetEditCamera
-          |. GameObjectComponentEngineService.unsafeGetArcballCameraControllerComponent(
-               engineState,
-             )
-          |. ArcballCameraEngineService.unsafeGetArcballCameraControllerDistance(
-               engineState,
-             )
+          (editorState |> SceneViewEditorService.unsafeGetEditCamera)
+          ->(
+              GameObjectComponentEngineService.unsafeGetArcballCameraControllerComponent(
+                engineState,
+              )
+            )
+          ->(
+              ArcballCameraEngineService.unsafeGetArcballCameraControllerDistance(
+                engineState,
+              )
+            )
           |> FloatService.truncateFloatValue(_, 3);
 
         let _getTarget = ((editorState, engineState)) =>
-          editorState
-          |> SceneViewEditorService.unsafeGetEditCamera
-          |. GameObjectComponentEngineService.unsafeGetArcballCameraControllerComponent(
-               engineState,
-             )
-          |. ArcballCameraEngineService.unsafeGetArcballCameraControllerTarget(
-               engineState,
-             )
+          (editorState |> SceneViewEditorService.unsafeGetEditCamera)
+          ->(
+              GameObjectComponentEngineService.unsafeGetArcballCameraControllerComponent(
+                engineState,
+              )
+            )
+          ->(
+              ArcballCameraEngineService.unsafeGetArcballCameraControllerTarget(
+                engineState,
+              )
+            )
           |> Vector3Service.truncate(3);
 
         describe(
@@ -209,10 +233,14 @@ let _ =
 
               let engineState =
                 MainEditorSceneTool.unsafeGetScene()
-                |. GameObjectComponentEngineService.unsafeGetTransformComponent(
-                     engineState,
-                   )
-                |. TransformEngineService.setScale((3., 1., 1.), engineState);
+                ->(
+                    GameObjectComponentEngineService.unsafeGetTransformComponent(
+                      engineState,
+                    )
+                  )
+                ->(
+                    TransformEngineService.setScale((3., 1., 1.), engineState)
+                  );
               let firstChild = MainEditorSceneTool.getFirstCube(engineState);
               let secondChild =
                 MainEditorSceneTool.getSecondCube(engineState);
@@ -248,10 +276,14 @@ let _ =
 
               let engineState =
                 firstChild
-                |. GameObjectComponentEngineService.unsafeGetTransformComponent(
-                     engineState,
-                   )
-                |. TransformEngineService.setScale((3., 1., 1.), engineState);
+                ->(
+                    GameObjectComponentEngineService.unsafeGetTransformComponent(
+                      engineState,
+                    )
+                  )
+                ->(
+                    TransformEngineService.setScale((3., 1., 1.), engineState)
+                  );
               let engineState =
                 engineState
                 |> TransformGameObjectEngineService.setLocalPosition(

commit 0d5fba172f61d76d9f1fdab0acc89038fcbf2329

feat(hotkey): fix asset header->remove caused by 05c98b5fc1c3c769098b888120c1011724c5f29d

src/core/composable_component/mainEditor/composable_component/bottom_components/composable_component/project/composable_component/asset/composable_component/header/ui/MainEditorAssetHeader.re
@@ -51,16 +51,16 @@ let _renderRemoveItem =
       _e =>
         CurrentNodeIdAssetEditorService.couldRemoveCurrentNode
         |> StateLogicService.getEditorState ?
-          () : Method.removeAssetNode((uiState, dispatchFunc), (), ())
+          Method.removeAssetNode((uiState, dispatchFunc), (), ()) : ()
     }>
     {
       CurrentNodeIdAssetEditorService.couldRemoveCurrentNode
       |> StateLogicService.getEditorState ?
-        <div className="item-notBeClick">
-          <img src="./public/img/notRemove.png" />
-        </div> :
         <div className="item-canBeClick">
           <img src="./public/img/remove.png" />
+        </div> :
+        <div className="item-notBeClick">
+          <img src="./public/img/notRemove.png" />
         </div>
     }
   </div>;